0983b148a79255cb5e940b36886e458585ee06e9
Ihor Savenko <csigorek@gmail.com>
2024-04-01 00:16:52 +0100

    responsive

--- src/app/components/InputCheck.tsx
+++ src/app/components/InputCheck.tsx
@@ -1,11 +1,10 @@
 'use client';
 import React, { useState } from 'react';
-import {Input, Divider, Button, Link} from "@nextui-org/react";
+import { Input, Divider, Button, Link } from "@nextui-org/react";
 
 const InputCheck = ({ newcomers, database }) => {
   const [inputValue, setInputValue] = useState('');
   const [status, setStatus] = useState('');
-
 
   const handleSubmit = () => {
     if (newcomers.includes(inputValue)) {
@@ -19,25 +18,25 @@ const InputCheck = ({ newcomers, database }) => {
 
   return (
     <div className="input-check flex justify-center flex-col m-auto h-screen">
-      <p className='font-bold leading-7 sm:tracking-tight p-10' style={{fontSize: 47, fontFamily: "Orbitron"}}>Check if you're coming to Mars!</p>
-      <Divider className='ml-10 mr-10'/>
-         <div className='columns-2 p-10'>
-           <Input
-          className=' max-w-sm'
-        placeholder="Instagram Username"
-        value={inputValue}
-        onValueChange={setInputValue}
-            startContent={
+      <p className='font-bold leading-10 sm:leading-7 sm:tracking-tight p-4 sm:p-10' style={{ fontSize: 'clamp(2rem, 5vw, 3rem)', fontFamily: "Orbitron", lineHeight: '1.5' }}>Check if you're coming to Mars!</p>
+      <Divider className='ml-10 mr-10' />
+      <div className='columns-2 p-4 sm:p-10'>
+        <Input
+          className='max-w-sm'
+          placeholder="Instagram Username"
+          value={inputValue}
+          onValueChange={setInputValue}
+          startContent={
             <div className="pointer-events-none flex items-center">
               <span className="text-default-400 text-small">@</span>
             </div>
           }
-      />
-      <Button onClick={handleSubmit} style={{fontFamily: "Orbitron", fontWeight: 600}}>Check Me!</Button>
-         </div>
-      {status ? <p className='ml-10' style={{fontSize: 20, fontFamily: "Orbitron"}}>{status}</p> : <p className='ml-10' style={{fontSize: 15, fontFamily: "Orbitron"}}>Please enter your Instagram username in the field above and click 'Check Me!' to get current status about your travel to Mars.</p>}
+        />
+        <Button onClick={handleSubmit} style={{ fontFamily: "Orbitron", fontWeight: 600 }}>Check Me!</Button>
+      </div>
+      {status ? <p className='ml-10 mt-2 sm:mt-4' style={{ fontSize: 'clamp(1.2rem, 2vw, 1.5rem)', fontFamily: "Orbitron" }}>{status}</p> : <p className='ml-10 mt-2 sm:mt-4' style={{ fontSize: 'clamp(1rem, 2vw, 1.2rem)', fontFamily: "Orbitron" }}>Please enter your Instagram username in the field above and click 'Check Me!' to get current status about your travel to Mars.</p>}
 
-      <Link isBlock showAnchorIcon href="https://instagram.com/integrityofstellar" color="warning" className="ml-10 mt-10 max-w-fit">
+      <Link isBlock showAnchorIcon href="https://instagram.com/integrityofstellar" color="warning" className="ml-10 mt-4 max-w-fit">
         Instagram
       </Link>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,18 @@ export default function Home() {
   const database = databaseData;
 
   return (
-     <main className="">
-         <video autoPlay muted loop className="video">         
-         <source src="/bg.mp4" type="video/mp4"/>       
+    <main className="">
+      <video autoPlay muted loop className="video">         
+        <source src="/bg.mp4" type="video/mp4"/>       
       </video>
       <div className="container">
-        <div className="columns">
-          <UserList usernames={newcomers} />
-        </div>
-        <div className="columns max-w-4xl">
-          <InputCheck newcomers={newcomers} database={database} />
-          
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+          <div className="lg:col-span-2">
+            <InputCheck newcomers={newcomers} database={database} />
+          </div>
+          <div>
+            <UserList usernames={newcomers} />
+          </div>
         </div>
       </div>
     </main>
